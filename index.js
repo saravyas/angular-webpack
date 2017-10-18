34da72ebc9221f54f19aea373c5e3b037e7570a9
saravyas <saravyas007@gmail.com>
2017-10-18 20:56:38 +0530

    Angular with webpack

---
+++ index.js
@@ -0,0 +1,3 @@
+require("./style.css");
+var moment = require('moment');
+console.log(moment().format('MMMM Do YYYY hh:mm:ss a'));
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = function() {
+   return {	
+	"entry" : {
+		main : "./index.js",
+	},
+	"output" : {
+		filename : "[chunkhash].[name].js",
+		path : path.resolve(__dirname, 'dist')
+	},
+	plugins : [
+	  new webpack.optimize.CommonsChunkPlugin({
+	  	name : 'vendor',
+	  	minCunks: function(module){
+	  		return module.context && module.context.indexOf('node_modules') >= 0 && count >= 2
+	  	}
+	  })	
+	],
+	module : {
+	  loaders : [
+	  	{ test : /\.css/, loader:"style-loader!css-loader" },
+	  	{ test : /\.png/, loader:"url-loader?limit=100000&mimetype=image/png" },
+	  	{ test : /\.jpg/, loader:"file-loader"}
+	  ]
+	}
+   }
+}
